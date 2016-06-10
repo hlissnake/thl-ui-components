b7e936e4762f4594221359eb1577ad822fd9ee50
simeonc <simeon.cheeseman@thlonline.com>
2016-06-10 17:49:00 +1200

    fix(exports): Fix export for ToolbarOverlay

--- index.jsx
+++ index.jsx
@@ -2,6 +2,7 @@ import * as Stateless from './Stateless';
 
 export { default as GeneratedForm } from './GeneratedForm';
 export { default as NavigationToolbar } from './NavigationToolbar';
+export { default as ToolbarOverlay } from './ToolbarOverlay';
 export { default as RebassThemes } from './RebassThemes';
 export { default as VersionedComponent } from './VersionedComponent';
 export { Stateless };
