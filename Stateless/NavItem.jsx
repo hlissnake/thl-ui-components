89de06eacd68a1a53a4cb6336336a2d92399d049
simeonc <simeon.cheeseman@thlonline.com>
2016-09-21 16:31:15 +1200

    fix(NavItem): Make NavItem truly stateless

--- Stateless/NavItem.jsx
+++ Stateless/NavItem.jsx
@@ -2,11 +2,9 @@ import React, {Component} from 'react';
 import {Link} from 'react-router';
 import {NavItem} from 'rebass';
 
-export default class WrappedNavItem extends Component {
-	render() {
-		return <NavItem
-			{...this.props}
-			is={this.props.is || Link}
-		/>;
-	}
+export default function WrappedNavItem(props) {
+	return <NavItem
+		{...props}
+		is={props.is || Link}
+	/>;
 }
