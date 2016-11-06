15edd6c0bb0f1fffe42c648a9c0c4eea407a62a6
simeonc <simeon.cheeseman@thlonline.com>
2016-11-07 11:11:42 +1300

    fix(PanelHeader): Color for theme default

--- Stateless/PanelHeader.jsx
+++ Stateless/PanelHeader.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PanelHeader from 'rebass/dist/PanelHeader';
 
 function WrappedPanelHeader({style = {}, theme = 'primary', ...props}, {rebass}) {
-	if (props.inverted) {
+	if (props.inverted || theme === 'default') {
 		style.color = rebass.colors.black;
 	}
 	return <PanelHeader
